1206a96aae50aa1d42d6ba92e9eba618a04c462f
Briar Bowser <briarbear7@gmail.com>
2015-10-20 15:59:21 -0500

    Now uses app compatibility libraries for notification.

--- src/android/MediaNotification.java
+++ src/android/MediaNotification.java
@@ -20,16 +20,16 @@
 package org.apache.cordova.media;
 
 import android.app.PendingIntent;
-import android.media.session.MediaSession;
-import android.media.MediaMetadata;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.MediaMetadataCompat;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Bitmap;
 
-import android.app.Notification;
+import android.support.v7.app.NotificationCompat;
 import android.app.NotificationManager;
-import android.media.session.PlaybackState;
+import android.support.v4.media.session.PlaybackStateCompat;
 
 import android.view.KeyEvent;
 
@@ -53,11 +53,11 @@ public class MediaNotification extends BroadcastReceiver {
     final private AudioPlayer player;
     private Context context;
     private NotificationManager mNotificationManager;
-    private MediaSession mSession;
+    private MediaSessionCompat mSession;
     private int mState;
     final private CreateOptions options;
-    private Notification.Builder mNotificationBuilder;
-    private Notification.Action mPlayPauseAction;
+    private NotificationCompat.Builder mNotificationBuilder;
+    private NotificationCompat.Action mPlayPauseAction;
 
     public MediaNotification() {
         this.player = null;
@@ -75,18 +75,18 @@ public class MediaNotification extends BroadcastReceiver {
         Log.d(TAG, "Showing notification");
         Bitmap artwork = options.getIconBitmap();
 
-        this.mSession = new MediaSession(this.context, "Satchel Media Session");
-        this.mSession.setMetadata(new MediaMetadata.Builder()
-            .putBitmap(MediaMetadata.METADATA_KEY_ALBUM_ART, artwork)
-            .putString(MediaMetadata.METADATA_KEY_ARTIST, this.options.getPodcastName())
-            .putString(MediaMetadata.METADATA_KEY_TITLE, this.options.getEpisodeName())
+        this.mSession = new MediaSessionCompat(this.context, "Satchel Media Session");
+        this.mSession.setMetadata(new MediaMetadataCompat.Builder()
+            .putBitmap(MediaMetadataCompat.METADATA_KEY_ALBUM_ART, artwork)
+            .putString(MediaMetadataCompat.METADATA_KEY_ARTIST, this.options.getPodcastName())
+            .putString(MediaMetadataCompat.METADATA_KEY_TITLE, this.options.getEpisodeName())
             .build());
 
         this.mSession.setActive(true);
-        this.mSession.setFlags(MediaSession.FLAG_HANDLES_TRANSPORT_CONTROLS | MediaSession.FLAG_HANDLES_MEDIA_BUTTONS);
+        this.mSession.setFlags(MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS | MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS);
 
         // Attach a new Callback to receive MediaSession updates
-        this.mSession.setCallback(new MediaSession.Callback() {
+        this.mSession.setCallback(new MediaSessionCompat.Callback() {
             public boolean onMediaButtonEvent(Intent mediaButtonIntent) {
                 if(mSession == null || !Intent.ACTION_MEDIA_BUTTON.equals(mediaButtonIntent.getAction())) {
                     return false;
@@ -96,9 +96,6 @@ public class MediaNotification extends BroadcastReceiver {
                 if (ke == null || ke.getAction() != KeyEvent.ACTION_DOWN) {
                     return false;
                 }
-
-                //PlaybackState state = mPlaybackState;
-                //long validActions = state == null ? 0 : state.getActions();
 
                 if(ke.getKeyCode() == KeyEvent.KEYCODE_MEDIA_PLAY) {
                     player.startPlaying(options.getSourcePath());
@@ -114,38 +111,26 @@ public class MediaNotification extends BroadcastReceiver {
             }
         });
 
-        /*PlaybackState state = new PlaybackState.Builder()
-        .setActions(
-                PlaybackState.ACTION_PLAY | PlaybackState.ACTION_PLAY_PAUSE |
-                PlaybackState.ACTION_PLAY_FROM_MEDIA_ID | PlaybackState.ACTION_PAUSE |
-                PlaybackState.ACTION_SKIP_TO_NEXT | PlaybackState.ACTION_SKIP_TO_PREVIOUS)
-        .setState(PlaybackState.STATE_PAUSED, PlaybackState.PLAYBACK_POSITION_UNKNOWN, 0, SystemClock.elapsedRealtime())
-        .build();
-
-        this.mSession.setPlaybackState(state);*/
-
-        this.mState = PlaybackState.STATE_BUFFERING;
+        this.mState = PlaybackStateCompat.STATE_BUFFERING;
         this.updateState();
 
         this.updatePlayPauseAction();
 
-        mNotificationBuilder = new Notification.Builder(this.context)
-             .setShowWhen(false)
-             .setStyle(new Notification.MediaStyle()
-                 .setMediaSession(this.mSession.getSessionToken())
-                 .setShowActionsInCompactView(0, 1, 2))
-             .setColor(Color.parseColor("#303030"))
-             .setSmallIcon(this.options.getSmallIcon())
-             .setLargeIcon(artwork)
-             .setVisibility(Notification.VISIBILITY_PUBLIC)
-             .setContentText(this.options.getEpisodeName())
-             .setContentTitle(this.options.getPodcastName())
-             .setProgress(100, 33, false)
-             .setOngoing(false)
-             .addAction(this.options.getMediaBackIcon(), "prev", retrievePlaybackAction(ACTION_REWIND))
-             //.addAction(android.R.drawable.ic_media_play, "play", retrievePlaybackAction(ACTION_PLAY))
-             .addAction(mPlayPauseAction)
-             .addAction(this.options.getMediaForwardIcon(), "next", retrievePlaybackAction(ACTION_FORWARD));
+        mNotificationBuilder = new NotificationCompat.Builder(this.context);
+        mNotificationBuilder.setShowWhen(false);
+        mNotificationBuilder.setColor(Color.parseColor("#303030"));
+        mNotificationBuilder.setSmallIcon(this.options.getSmallIcon());
+        mNotificationBuilder.setLargeIcon(artwork);
+        mNotificationBuilder.setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+        mNotificationBuilder.setContentText(this.options.getEpisodeName());
+        mNotificationBuilder.setContentTitle(this.options.getPodcastName());
+        mNotificationBuilder.setOngoing(false);
+        mNotificationBuilder .addAction(this.options.getMediaBackIcon(), "prev", retrievePlaybackAction(ACTION_REWIND));
+        mNotificationBuilder.addAction(mPlayPauseAction);
+        mNotificationBuilder.addAction(this.options.getMediaForwardIcon(), "next", retrievePlaybackAction(ACTION_FORWARD));
+        mNotificationBuilder.setStyle(new NotificationCompat.MediaStyle()
+            .setMediaSession(this.mSession.getSessionToken())
+            .setShowActionsInCompactView(0, 1, 2));
 
          mNotificationManager.notify(1, mNotificationBuilder.build());
     }
@@ -154,7 +139,7 @@ public class MediaNotification extends BroadcastReceiver {
         Log.d(TAG, "updatePlayPauseAction");
         String playPauseLabel = "";
         int playPauseIcon;
-        if (this.mState == PlaybackState.STATE_PLAYING) {
+        if (this.mState == PlaybackStateCompat.STATE_PLAYING) {
             playPauseLabel = "Pause";
             playPauseIcon = this.options.getMediaPauseIcon();
         } else {
@@ -163,7 +148,7 @@ public class MediaNotification extends BroadcastReceiver {
         }
 
         if (mPlayPauseAction == null) {
-            mPlayPauseAction = new Notification.Action(playPauseIcon, playPauseLabel, retrievePlaybackAction(ACTION_PAUSE));
+            mPlayPauseAction = new NotificationCompat.Action(playPauseIcon, playPauseLabel, retrievePlaybackAction(ACTION_PAUSE));
         } else {
             mPlayPauseAction.icon = playPauseIcon;
             mPlayPauseAction.title = playPauseLabel;
@@ -189,21 +174,21 @@ public class MediaNotification extends BroadcastReceiver {
         Log.d(TAG, "Updating state to: " + state);
 
         if(state == AudioPlayer.STATE.MEDIA_STARTING) {
-            this.mState = PlaybackState.STATE_PAUSED;
+            this.mState = PlaybackStateCompat.STATE_PAUSED;
         }
         else if(state == AudioPlayer.STATE.MEDIA_RUNNING) {
-            this.mState = PlaybackState.STATE_PLAYING;
+            this.mState = PlaybackStateCompat.STATE_PLAYING;
         }
         else if(state == AudioPlayer.STATE.MEDIA_STOPPED) {
-            this.mState = PlaybackState.STATE_STOPPED;
+            this.mState = PlaybackStateCompat.STATE_STOPPED;
         }
         else if(state == AudioPlayer.STATE.MEDIA_PAUSED) {
-            this.mState = PlaybackState.STATE_PAUSED;
+            this.mState = PlaybackStateCompat.STATE_PAUSED;
         }
 
-        long position = PlaybackState.PLAYBACK_POSITION_UNKNOWN;
+        long position = PlaybackStateCompat.PLAYBACK_POSITION_UNKNOWN;
 
-        if (mState == PlaybackState.STATE_PLAYING) {
+        if (mState == PlaybackStateCompat.STATE_PLAYING) {
             position = this.player.getCurrentPosition();
             mNotificationBuilder.setOngoing(true);
         }
@@ -211,8 +196,8 @@ public class MediaNotification extends BroadcastReceiver {
             mNotificationBuilder.setOngoing(false);
         }
 
-        PlaybackState.Builder stateBuilder = new PlaybackState.Builder()
-                .setActions(getAvailableActions());
+        PlaybackStateCompat.Builder stateBuilder = new PlaybackStateCompat.Builder();
+        stateBuilder.setActions(getAvailableActions());
         stateBuilder.setState(mState, position, 1.0f);
 
         this.mSession.setPlaybackState(stateBuilder.build());
@@ -223,14 +208,14 @@ public class MediaNotification extends BroadcastReceiver {
     }
 
     private long getAvailableActions() {
-        long actions = PlaybackState.ACTION_PLAY |
-                PlaybackState.ACTION_PLAY_FROM_MEDIA_ID |
-                PlaybackState.ACTION_PLAY_FROM_SEARCH |
-                PlaybackState.ACTION_SKIP_TO_PREVIOUS |
-                PlaybackState.ACTION_SKIP_TO_NEXT;
+        long actions = PlaybackStateCompat.ACTION_PLAY |
+                PlaybackStateCompat.ACTION_PLAY_FROM_MEDIA_ID |
+                PlaybackStateCompat.ACTION_PLAY_FROM_SEARCH |
+                PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS |
+                PlaybackStateCompat.ACTION_SKIP_TO_NEXT;
 
-        if (mState == PlaybackState.STATE_PLAYING) {
-            actions |= PlaybackState.ACTION_PAUSE;
+        if (mState == PlaybackStateCompat.STATE_PLAYING) {
+            actions |= PlaybackStateCompat.ACTION_PAUSE;
         }
 
         return actions;
